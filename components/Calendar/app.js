2acfae731007c695789f22b88d674a836a13846c
Andrus Asumets <andrus.asumets@gmail.com>
2016-07-23 16:51:52 +0300

    Calendar test 003

--- components/Calendar/app.js
+++ components/Calendar/app.js
@@ -1,10 +1,31 @@
 import { Component } from 'react';
 import Calendar from 'rc-calendar';
+import Timeline from 'react-calendar-timeline'
+import moment from 'moment'
+
+const groups = [
+  {id: 1, title: 'group 1'},
+  {id: 2, title: 'group 2'}
+]
+
+const items = [
+  {id: 1, group: 1, title: 'item 1', start_time: moment(), end_time: moment().add(1, 'hour')},
+  {id: 2, group: 2, title: 'item 2', start_time: moment().add(-0.5, 'hour'), end_time: moment().add(0.5, 'hour')},
+  {id: 3, group: 1, title: 'item 3', start_time: moment().add(2, 'hour'), end_time: moment().add(3, 'hour')}
+]
 
 export default class Content extends Component {
     render() {
         return (
-            <Calendar />
+            <div>
+                <Calendar />
+                Rendered by react!
+                <Timeline groups={groups}
+                          items={items}
+                          defaultTimeStart={moment().add(-12, 'hour')}
+                          defaultTimeEnd={moment().add(12, 'hour')}
+                          />
+              </div>
         )
     }
 }

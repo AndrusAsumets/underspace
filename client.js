02766a7fdc3c41ae070ba96df43c173c216bf09d
Andrus Asumets <andrus.asumets@gmail.com>
2016-07-24 20:34:22 +0300

    Escape re-write

--- client.js
+++ client.js
@@ -20,32 +20,31 @@ var App = React.createClass({
         tree = [].slice.call(tree)
         tree.forEach(function(branch) {
             const component = branch.getAttribute('u-component')
-            const id = branch.id
 
-            async function load(component, id) {
+            async function load(component, branch) {
                 if (!window[component]) {
                     window[component] = 'fetching'
                     const res = await fetch('/components/' + component)
                     const text = await res.text()
                     new Function(text)()
-                    elements.push({ component: component, id: id })
+                    elements.push({ component: component, branch: branch })
                     components[component] = window[component].default
                     self.setState({ elements: elements, component: components })
                 }
 
                 else if (window[component] === 'fetching') {
                     setTimeout(function() {
-                        load(component, id)
+                        load(component, branch)
                     }, DELAY)
                 }
 
                 else if (window[component].default) {
-                    elements.push({ component: component, id: id })
+                    elements.push({ component: component, branch: branch })
                     self.setState({ elements: elements })
                 }
             }
 
-            load(component, id)
+            load(component, branch)
         })
     },
     render() {
@@ -54,7 +53,7 @@ var App = React.createClass({
         return (
             <div className="root">
                 {[...elements].map((element, i) =>
-                     <Display key={ i } component={ components[element.component] } id={ element.id } />
+                     <Display key={ i } component={ components[element.component] } branch={ element.branch } />
                 )}
             </div>
         )
@@ -64,7 +63,7 @@ var App = React.createClass({
 var Display = React.createClass({
     render() {
         return (
-            <Escape id={ this.props.id }>
+            <Escape branch={ this.props.branch }>
                 <this.props.component />
             </Escape>
         )
@@ -78,35 +77,24 @@ ReactDOM.render(
 
 var Escape = React.createClass({
     render: function() {
-        this._nodes = this.renderLayer()
+        this._nodes = this.props.children
+
         return <noscript ref="from"/>
     },
-    renderLayer: function() {
-        var { ...props } = this.props
-        return <div {...props}>
-                {this.props.children}
-            </div>
-    },
     componentDidMount: function() {
-        const { id } = this.props
         this.escapePoint = this.refs.from.parentNode
-        var layer = document.createElement('div')
-        var parent = document.getElementById(id)
-        parent.appendChild(layer);
-        this._layer = layer
+
         ReactDOM.render(
             this._nodes,
-            this._layer)
+            this.props.branch)
     },
     componentDidUpdate: function() {
         ReactDOM.render(
             this._nodes,
-            this._layer)
+            this.props.branch)
     },
     componentWillUnmount: function() {
         this.escapePoint = undefined
-        ReactDOM.unmountComponentAtNode(this._layer)
-        this._layer.parentNode.removeChild(this._layer)
-        this._layer = null
+        ReactDOM.unmountComponentAtNode(this.props.branch)
     },
 });

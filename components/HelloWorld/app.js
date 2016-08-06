2f27593f4df61188c2a02c11e44de8a587700217
Andrus Asumets <andrus.asumets@gmail.com>
2016-08-06 14:24:31 +0300

    HTML

--- components/HelloWorld/app.js
+++ components/HelloWorld/app.js
@@ -7,8 +7,6 @@ export default class Content extends Component {
                 <b>
                     <h1>Hello, World!</h1>
                 </b>
-
-                <h3>underspace is a proof of concept demo, which allows for dynamically injecting React components into DOM by writing nothing but pure HTML. In another words, WordPress on React.</h3>
             </div>
         )
     }
